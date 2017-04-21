70af859afcb4ce0d44882e62daf3fc4ce59e0875
tresis <tresis@tresiss-Air.home>
2017-04-21 06:53:35 +0200

    second changes

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ app.controller("ethCtrl", function($http, $scope){
   //api data 
 	$http.get("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken")
   .then(function(response) {
-    // price data attribute
+    // price data 
     $scope.price = response.data;
   });
   //add a reload function 
